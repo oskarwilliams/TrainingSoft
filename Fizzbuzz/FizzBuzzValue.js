fac43865523eab9f17068ad3ae7710c76fb9c4b3
Oskar Williams <Oskar.Williams@softwire.com>
2019-07-16 08:51:57 +0100

    Included git ignore

---
+++ Fizzbuzz/FizzBuzzValue.js
@@ -0,0 +1,28 @@
+exports.Calculate = function(i,rules) {
+    let s = []
+    if (i % 3 == 0 && rules.includes(3)){
+        s.push('Fizz')        
+    }
+    if (i % 13 == 0 && rules.includes(13)) {
+        s.push('Fezz')
+    }
+    if (i % 5 == 0 && rules.includes(5)) {
+        s.push('Buzz')
+    }
+    if (i % 7 == 0 && rules.includes(7)) {
+        s.push('Bang')
+    }
+    if (i % 11 == 0 && rules.includes(11)) {
+        s = ['Bong']
+    }
+    if (i % (11 * 13) == 0 && rules.includes(11) && rules.includes(13)) {
+        s = ['FezzBong']
+    }
+    if (i % 17 == 0 && rules.includes(17)) {
+        s.reverse()
+    }
+    if (s.length == 0) {
+        s = [i]
+    }
+    return s    
+}
--- Fizzbuzz/index.js
+++ Fizzbuzz/index.js
@@ -1,4 +1,5 @@
 var readlineSync = require('readline-sync')
+const FizzBuzzValue = require('./FizzBuzzValue')
 
 function getMaxnum(){
     const maxnum = Number(readlineSync.question('What is the maximum number?  '));
@@ -9,40 +10,8 @@ function getMaxnum(){
     return maxnum
 }
 
-
-function FizzBuzz(i,rules) {
-    let s = []
-    if (i % 3 == 0 && rules.includes(3)){
-        s.push('Fizz')        
-    }
-    if (i % 13 == 0 && rules.includes(13)) {
-        s.push('Fezz')
-    }
-    if (i % 5 == 0 && rules.includes(5)) {
-        s.push('Buzz')
-    }
-    if (i % 7 == 0 && rules.includes(7)) {
-        s.push('Bang')
-    }
-    if (i % 11 == 0 && rules.includes(11)) {
-        s = ['Bong']
-    }
-    if (i % (11 * 13) == 0 && rules.includes(11) && rules.includes(13)) {
-        s = ['FezzBong']
-    }
-    if (i % 17 == 0 && rules.includes(17)) {
-        s.reverse()
-    }
-    if (s.length == 0) {
-        s = [i]
-    }
-    return s    
-}
-
-
 const maxnum = getMaxnum()
 const rules = readlineSync.question('What number rules do you want? (separate by commas)  ').split(',').map(Number);
-console.log(rules)
 for (let i = 1; i < +maxnum + 1; i++){
-    console.log(FizzBuzz(i,rules).join(''))
+    console.log(FizzBuzzValue.Calculate(i,rules).join(''))
 }
