9ca95e7a6c3e72197f636017b5ed0798eaee9f30
Oskar Williams <Oskar.Williams@softwire.com>
2019-07-18 17:03:09 +0100

    Version 2.1, formatting

--- BusBoard/index.js
+++ BusBoard/index.js
@@ -19,16 +19,16 @@ function compare(a, b) {
 async function getStopList() {
     const postCode = readlineSync.question('What is the postcode?    ');
     postCode.replace(/\s/g, "");
-    let websitePostCode = new Website('http://api.postcodes.io/postcodes/' + postCode);
-    let postCodeData = await websitePostCode.getData();
-    let lon = postCodeData.result.longitude;
-    let lat = postCodeData.result.latitude;
-    let radius = 300;
-    let websiteStopCode = new Website(`https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanOnstreetBusCoachStopPair,NaptanOnstreetBusCoachStopCluster&radius=${radius}&modes=bus&lat=${lat}&lon=${lon}&app_id=f9b3dd07&app_key=89faa5803f90f1a589d6f12b34120bd1`);
-    let stopCodeData = await websiteStopCode.getData();
-    let stopList = [];
+    const websitePostCode = new Website('http://api.postcodes.io/postcodes/' + postCode);
+    const postCodeData = await websitePostCode.getData();
+    const lon = postCodeData.result.longitude;
+    const lat = postCodeData.result.latitude;
+    const radius = 300;
+    const websiteStopCode = new Website(`https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanOnstreetBusCoachStopPair,NaptanOnstreetBusCoachStopCluster&radius=${radius}&modes=bus&lat=${lat}&lon=${lon}&app_id=f9b3dd07&app_key=89faa5803f90f1a589d6f12b34120bd1`);
+    const stopCodeData = await websiteStopCode.getData();
+    const stopList = [];
     await stopCodeData.stopPoints.forEach(stop => {
-        let stopPoint = new Stop(stop.children[0].id, stop.children[0].commonName, stop.children[0].stopLetter, stop.distance);
+        const stopPoint = new Stop(stop.children[0].id, stop.children[0].commonName, stop.children[0].stopLetter, stop.distance);
         stopList.push(stopPoint);
     });
     return stopList;
@@ -36,19 +36,19 @@ async function getStopList() {
 
 async function getBusList(stopCode) {
     const website = new Website(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals?app_id=f9b3dd07&app_key=%89faa5803f90f1a589d6f12b34120bd1`);
-    let busList = [];
-    let arrivalsList = await website.getData();
+    const busList = [];
+    const arrivalsList = await website.getData();
     arrivalsList.forEach(arrival => {
-        let bus = new Bus(arrival.vehicleId, arrival.lineName, arrival.timeToStation, arrival.destinationName);
+        const bus = new Bus(arrival.vehicleId, arrival.lineName, arrival.timeToStation, arrival.destinationName);
         busList.push(bus);
     });
     return busList;
 }
 
 async function getSortedBusLists(stopList) {
-    let busLists = [];
+    const busLists = [];
     for (let i = 0; i < stopList.length; i++) {
-        let busList = await getBusList(stopList[i].id);
+        const busList = await getBusList(stopList[i].id);
         busLists.push(busList.sort(compare));
     }
     return busLists;
@@ -56,7 +56,7 @@ async function getSortedBusLists(stopList) {
 
 async function doTheThing() {
     const stopList = await getStopList();
-    let busLists = await getSortedBusLists(stopList);
+    const busLists = await getSortedBusLists(stopList);
     console.log('\n\n');
     console.log(stopList[0].name + '\n');
     console.table(busLists[0], ['line', 'timeToStationMins', 'destination']);
