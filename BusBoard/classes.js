44dfd3a9d4d793e6e9f1b4543ff4bf31894ba172
Oskar Williams <Oskar.Williams@softwire.com>
2019-07-18 16:39:07 +0100

    Version 2.0, nice and neat

--- BusBoard/classes.js
+++ BusBoard/classes.js
@@ -1,17 +1,44 @@
-class Bus{
-    constructor(id, line, timeToStation, destination){
+const request = require('request-promise');
+
+class Bus {
+    constructor(id, line, timeToStation, destination) {
         this.id = id;
         this.line = line;
         this.timeToStation = timeToStation;
         this.destination = destination;
+        this.timeToStationMins = this.getTimeToStationMins();
     }
 
-    get goodTime() {
+    getTimeToStationMins() {
         let minutes = Math.floor(this.timeToStation / 60);
         let seconds = this.timeToStation - minutes * 60;
-        if (seconds<10){seconds = '0' + seconds;}
-        return minutes + ':' + seconds;
+        if (seconds < 10) { seconds = '0' + seconds; }
+        return (minutes + ':' + seconds);
+    }
+}
+
+class Stop {
+    constructor(id, name, letter, distance) {
+        this.id = id;
+        this.name = name;
+        this.letter = letter;
+        this.distance = distance;
+    }
+}
+
+class Website {
+    constructor(url) {
+        this.url = url;
+    }
+
+    async getData() {
+        let Data = await request(this.url, function (error, response, body) {
+            return body;
+        });
+        return await JSON.parse(Data);
     }
 }
 
 exports.Bus = Bus;
+exports.Stop = Stop;
+exports.Website = Website;
